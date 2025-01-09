9b292123f4a36e35786de4b1f598666038abb087
cha-law <char_law@outlook.com>
2025-01-09 21:33:57 +0000

    Calculator now stops user from entering multiple operators consecutively

--- script.js
+++ script.js
@@ -1,4 +1,46 @@
+// Code Functions
+
+
+function assignEventListeners() {
+    let buttons = document.querySelectorAll(".button");
+    buttons.forEach(function (button) {
+        button.addEventListener("click", buttonPressed);
+    });
+}
+
+function buttonPressed(event) {
+    const numbers = "0123456789"
+    const target = event.target;
+
+    if (target.id === "AC") {
+        resetCalc();
+    } else if (target.id === "C") {
+        removePrevious();
+    } else if (target.id === "OPERATE") {
+        let result = operate();
+    } else {
+        addToOperationsOutput(target);
+        operate();
+    }
+}
+
+function addToOperationsOutput(target) {
+    let splitOperation = operationOutput.textContent.split("");
+    if ( (splitOperation.at(-1) === "+") || (splitOperation.at(-1) === "-") || (splitOperation.at(-1) === "x") || (splitOperation.at(-1) === "÷") || (splitOperation.at(-1) === "%") ) {
+        return;
+    }
+    operationOutput.textContent += target.textContent;
+}
+
+function operate() {
+    const operationText = operationOutput.textContent;
+    let splitOperation = operationText.split("");
+
+    console.log(splitOperation)
+}
+
 // Operation Functions
+
 
 function add(first, second) {
     return first + second;
@@ -16,30 +58,20 @@ function divide(first, second) {
     return first / second;
 }
 
-// Code Functions
-
 function resetCalc() {
-    //
+    operationOutput.textContent = "";
+    resultOutput.textContent = "";
 }
 
-function addNumber(target) {
-    //
+function removePrevious(target) {
+    operationOutput.textContent = operationOutput.textContent.slice(0, -1);
 }
 
-function buttonPressed(event) {
-    let numbers = "0123456789"
-    let target = event.target;
-    console.log(target.id);
-    if (target.id in numbers) {
-        addNumber(target);
-    }
-}
+// Variables
 
-function assignEventListeners() {
-    let buttons = document.querySelectorAll(".button");
-    buttons.forEach(function (button) {
-        button.addEventListener("click", buttonPressed);
-    });
-}
+
+const operationOutput = document.querySelector(".operations");
+const resultOutput = document.querySelector(".result");
+
 
 assignEventListeners();
