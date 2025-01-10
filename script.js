632283ae0207476d4459d0938725fbf5c863e590
cha-law <char_law@outlook.com>
2025-01-10 20:06:31 +0000

    Added result calculations, still need to fix problems with making calculations with a previous result

--- script.js
+++ script.js
@@ -16,57 +16,128 @@ function buttonPressed(event) {
         resetCalc();
     } else if (target.id === "C") {
         removePrevious();
+        operate(operator, num1, num2)
     } else if (target.id === "OPERATE") {
-        let result = operate();
+        result = operate(operator, num1, num2);
     } else {
         addToOperationsOutput(target);
-        operate();
+        result = operate(operator, num1, num2);
+
+        if (result != "") {
+            addToResultOutput(result);
+        } else {
+            addToResultOutput("");
+        }
     }
 }
 
 function addToOperationsOutput(target) {
     const operations = "+-x÷%"
-    let splitOperation = operationOutput.textContent.split("");
 
-    if ((operations.includes(splitOperation.at(-1))) && ((operations.includes(target.textContent)))) {
-        return;
+    if (operations.includes(target.textContent)) {
+        // Preventing user from adding double operators.
+        if ((operatorPresent === true) || (num1 === "")) {
+            // Ensuring that the answers are added to the operator output if another calculation is made.
+            if (result != "") {
+                operatorPresent = false;
+                operationOutput.textContent = result;
+                num1 = result;
+                operator = "";
+                num2 = "";
+                result = operate(operator, num1, num2);
+                if (result != "") {
+                    addToResultOutput(result);
+                } else {
+                    addToResultOutput("");
+                }
+            } else {
+                return;
+            }
+        }
+        operatorPresent = true;
+        operator = target.textContent;
+    } else if (operatorPresent === true) {
+        num2 = num2 + target.textContent;
+    } else {
+        num1 = num1 + target.textContent;
     }
     operationOutput.textContent += target.textContent;
 }
 
-function operate() {
-    const operationText = operationOutput.textContent;
-    let splitOperation = operationText.split("");
+function addToResultOutput(result) {
+    const resultOutput = document.querySelector(".result");
+    resultOutput.textContent = result;
+}
 
-    console.log(splitOperation)
+function operate(operator, a, b) {
+    a = Number(a)
+    b = Number(b)
+    switch (operator) {
+        case "+":
+            return add(a,b);
+        case "-":
+            return subtract(a,b);
+        case "x":
+            return multiply(a,b);
+        case "÷":
+            return divide(a,b);
+        case "%":
+            return divide(a,b);
+        default:
+            return "";
+    }
 }
 
 // Operation Functions
 
 
-function add(first, second) {
-    return first + second;
+function add(a, b) {
+    return a + b;
 }
 
-function subtract(first, second) {
-    return first - second;
+function subtract(a, b) {
+    return a - b;
 }
 
-function multiply(first, second) {
-    return first * second;
+function multiply(a, b) {
+    return a * b;
 }
 
-function divide(first, second) {
-    return first / second;
+function divide(a, b) {
+    return a / b;
+}
+
+function remainder(a, b) {
+    return a % b;
 }
 
 function resetCalc() {
     operationOutput.textContent = "";
     resultOutput.textContent = "";
+    operator = "";
+    num1 = "";
+    num2 = "";
+    operatorPresent = false;
+    result = "";
 }
 
 function removePrevious(target) {
     operationOutput.textContent = operationOutput.textContent.slice(0, -1);
+
+    if (num2 != "") {
+        num2.slice(0, -1);
+    } else if (operator != "") {
+        operator.slice(0, -1);
+    } else {
+        num1.slice(0, -1);
+    }
+    result = operate(operator, num1, num2);
+
+    if (result != "") {
+        addToResultOutput(result);
+    } else {
+        addToResultOutput("");
+    }
 }
 
 // Variables
@@ -74,6 +145,12 @@ function removePrevious(target) {
 
 const operationOutput = document.querySelector(".operations");
 const resultOutput = document.querySelector(".result");
+
+let operator = "";
+let num1 = "";
+let num2 = "";
+let operatorPresent = false;
+let result = "";
 
 
 assignEventListeners();
